92f8015ddc814e41b952d740694cedccf94c15de
Long Pham <long20p@gmail.com>
2013-08-24 17:30:27 +0200

    Init

---
+++ Infrastructure/RegionNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure
+{
+    public static class RegionNames
+    {
+        public static string ToolbarRegion = "ToolbarRegion";
+        public static string ContentRegion = "ContentRegion";
+    }
+}
